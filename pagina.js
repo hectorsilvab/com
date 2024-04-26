9b7ee387ad0cfd46123c33a3886d829017367ef1
hectorsilvab <168146994+hectorsilvab@users.noreply.github.com>
2024-04-25 17:12:58 -0700

    Add files via upload

---
+++ pagina.js
@@ -0,0 +1,3 @@
+var nom=prompt("Introduzca su nombre (esta informacion sera registrada en la pagina, no se hara uso de esta misma con fines que no seas escolares)")
+
+alert("gracias por visitar la pagina")
